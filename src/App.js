3ba1e5d04ad3b6126ced7d687a49515bbda2e9e6
zhangxp <zhangxp@sinandata.com>
2019-03-06 22:41:21 +0800

    初始化

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  componentDidMount(){
+    axios.get("goods/goodsList").then(response => {
+      console.log('re',response)
+      
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        首页
       </div>
     );
   }
---
+++ src/setupProxy.js
@@ -0,0 +1,9 @@
+const proxy = require('http-proxy-middleware');
+module.exports = function(app) {
+    app.use(proxy("/goods", {
+      "target": "http://localhost:3000",
+      "secure": false,
+      "changeOrigin": true
+    }
+    ));
+}
